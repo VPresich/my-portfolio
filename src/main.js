6e10b8030a79033965fc46cffe66d02277be79ba
VPresich <valya.alexandrova.presich@gmail.com>
2024-04-13 01:11:40 +0200

    change main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // import './js/quote/quote-handles.js';
-// import './js/exercise-modal/exercise-modal-handles.js';
 import './js/experiences/experiences-handle.js';
 import './js/header/header.js';
 import './js/footer/footer-icons.js';
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,4 +1,4 @@
-import './js/quote/quote-handles.js';
+// import './js/quote/quote-handles.js';
 import './js/header/header-portfolio.js';
 import './js/portfolio/portfolio-handle.js';
 import './js/portfolio-modal/portfolio-modal-handles.js';
