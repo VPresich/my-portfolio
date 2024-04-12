df407422c0993e5f9c70712e963f73ff70807e6e
VPresich <valya.alexandrova.presich@gmail.com>
2024-04-12 19:58:27 +0200

    edit footer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 // import './js/quote/quote-handles.js';
-// import './js/exercise-filters/exercise-filters-handles.js';
 import './js/exercise-modal/exercise-modal-handles.js';
-// import './js/footer/footer.js';
-
 import './js/experiences/experiences-handle.js';
 import './js/header/header.js';
 import './js/footer/footer-icons.js';
